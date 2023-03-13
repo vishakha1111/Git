3cd354a013a324ad318d7d7806c66ad804be7de0
Vishakha Jadhav <jadhavvishakha1111@gmail.com>
2023-03-13 18:03:23 +0530

    Added diff.py file

---
+++ diff.py
@@ -0,0 +1,11 @@
+# This program adds two numbers
+
+num1 = 1.5
+num2 = 6.3
+
+# Add two numbers
+diff = num1 - num2
+
+# Display the sum
+print('The diff of {0} and {1} is {2}'.format(num1, num2, sum))
+print("The result has been displayed")
